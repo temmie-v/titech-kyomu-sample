e6b99609ec20d2193aa68f027afe5456cdf79ca5
ishikawa taiyo <ikw.t34@gmail.com>
2022-09-06 17:09:41 +0900

    create project

---
+++ titech-kyomu-sample/titech_kyomu_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  titech_kyomu_sampleApp.swift
+//  titech-kyomu-sample
+//
+//  Created by temmie on 2022/09/06.
+//
+
+import SwiftUI
+
+@main
+struct titech_kyomu_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
